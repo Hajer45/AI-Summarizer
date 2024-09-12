5803e6c6e4539b280e4474c91bae6d1eb7a5746a
unknown <hagartalbi@gmail.com>
2024-09-12 18:43:19 +0100

    adding intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import Summary from "./components/Summary"
+import Intro from "./components/Intro"
+import "./App.css"
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className="main">
+        <div className="gradient"/>
+      </div>
+      <div className="app">
+        <Intro/>
+        <Summary/>
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Intro.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+const Intro = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3 '>
+        <img src={logo} alt="logo" className='w-28 object-contain' />
+        <button type='button' className='btn-primary' onClick={()=>window.open('https://github.com/Hajer45/AI-Summarizer')} >
+          Github
+        </button>
+      </nav>
+      <h1 className='head_text'>
+        Summarize Articles with <br className='mx-md:hidden'/>
+        <span className='violet_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+      Make your reading easier with Summize, an open-source tool that converts long 
+      articles into brief, straightforward summaries.
+      </h2>
+    </header>
+  )
+}
+
+export default Intro
